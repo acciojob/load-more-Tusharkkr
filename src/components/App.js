212f7a613ea27a94ae52f4af471a4fec0ff71f78
Tushar kumar <narutoboi41@gmail.com>
2026-04-13 23:10:27 +0530

    nomore

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,42 @@
+
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const items = [
-  "Item 1","Item 2","Item 3","Item 4","Item 5",
-  "Item 6","Item 7","Item 8","Item 9","Item 10",
-  "Item 11","Item 12","Item 13","Item 14","Item 15",
-  "Item 16","Item 17","Item 18","Item 19","Item 20",
-  "Item 21","Item 22","Item 23","Item 24","Item 25",
-  "Item 26","Item 27","Item 28","Item 29","Item 30"
+  "Item 1",
+  "Item 2",
+  "Item 3",
+  "Item 4",
+  "Item 5",
+  "Item 6",
+  "Item 7",
+  "Item 8",
+  "Item 9",
+  "Item 10",
+  "Item 11",
+  "Item 12",
+  "Item 13",
+  "Item 14",
+  "Item 15",
+  "Item 16",
+  "Item 17",
+  "Item 18",
+  "Item 19",
+  "Item 20",
+  "Item 21",
+  "Item 22",
+  "Item 23",
+  "Item 24",
+  "Item 25",
+  "Item 26",
+  "Item 27",
+  "Item 28",
+  "Item 29",
+  "Item 30"
 ];
 
 const App = () => {
+
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
@@ -22,24 +48,22 @@ const App = () => {
   return (
     <div>
       <ul>
-        {
-          count > 0 &&
-          items.slice(0, count).map((value, index) => (
-            <li key={index}>{value}</li>
-          ))
-        }
+        {items.slice(0, count).map((value, index) => (
+          <li key={index}>{value}</li>
+        ))}
       </ul>
 
       <button onClick={handleClick}>Load more</button>
     </div>
-  );
-};
+  )
+}
 
-export default App;
-
-
+export default App
 
 
+
+
+//! My Code
 //   let [increment, setIncrement] = useState(-1)
 
 //   function handleClick() {
