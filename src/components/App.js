cee50f4a7b8d435a9cc16d16519ddd76ce35ea74
Tushar kumar <narutoboi41@gmail.com>
2026-04-13 20:59:10 +0530

    dekhoab

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,21 @@
-
 import React, { useState } from "react";
 import './../styles/App.css';
 
 const items = [
-  "Item 1",
-  "Item 2",
-  "Item 3",
-  "Item 4",
-  "Item 5",
-  "Item 6",
-  "Item 7",
-  "Item 8",
-  "Item 9",
-  "Item 10",
-  "Item 11",
-  "Item 12",
-  "Item 13",
-  "Item 14",
-  "Item 15",
-  "Item 16",
-  "Item 17",
-  "Item 18",
-  "Item 19",
-  "Item 20",
-  "Item 21",
-  "Item 22",
-  "Item 23",
-  "Item 24",
-  "Item 25",
-  "Item 26",
-  "Item 27",
-  "Item 28",
-  "Item 29",
-  "Item 30"
+  "Item 1","Item 2","Item 3","Item 4","Item 5",
+  "Item 6","Item 7","Item 8","Item 9","Item 10",
+  "Item 11","Item 12","Item 13","Item 14","Item 15",
+  "Item 16","Item 17","Item 18","Item 19","Item 20",
+  "Item 21","Item 22","Item 23","Item 24","Item 25",
+  "Item 26","Item 27","Item 28","Item 29","Item 30"
 ];
 
 const App = () => {
-
   const [count, setCount] = useState(0);
 
   const handleClick = () => {
-    if (count < items.length) {
-      setCount(count + 10);
-    }
+    const newCount = count + 10;
+    setCount(Math.min(newCount, items.length));
   };
 
   return (
@@ -55,11 +28,10 @@ const App = () => {
 
       <button onClick={handleClick}>Load more</button>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
+export default App;
 
 
 
